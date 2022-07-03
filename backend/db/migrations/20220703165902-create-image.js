8b65c92d6f9855c27c22df251995ffc46536e6ca
Jair Carbajal <jair.carbajal91@gmail.com>
2022-07-03 10:02:26 -0700

    created images table

---
+++ backend/db/migrations/20220703165902-create-image.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      reviewId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  }
+};
