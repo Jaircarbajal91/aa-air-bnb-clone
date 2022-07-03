94f6a719b05617f3a6a2f4ed77dc736f2afe059a
Jair Carbajal <jair.carbajal91@gmail.com>
2022-07-03 11:52:28 -0700

    1st attempt at creating associations

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -10,7 +10,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Review.belongsTo(models.User, {
+        foreignKey: 'userId'
+      })
+      Review.hasMany(models.Image, {
+        foreignKey: 'imageId'
+      })
+      Review.belongsTo(models.Spot, {
+        foreignKey: 'spotId'
+      })
     }
   }
   Review.init({
