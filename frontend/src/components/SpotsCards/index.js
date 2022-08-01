10cd799f8ad2144461a9a68f89212e9651555d0c
Jair Carbajal <jair.carbajal91@gmail.com>
2022-07-31 19:17:15 -0700

    added isLoaded to geAllSpots

--- frontend/src/components/SpotsCards/index.js
+++ frontend/src/components/SpotsCards/index.js
@@ -1,16 +1,17 @@
 import { useSelector, useDispatch } from "react-redux"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getAllSpots, getSpotDetails } from "../../store/spots"
 import Card from "../Cards"
 import './SpotsCards.css'
 
 function SpotCards() {
   const spots = useSelector(state => state.spots.orderedSpotsList)
+  const [isLoaded, setIsLoaded] = useState(false);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getAllSpots());
+    dispatch(getAllSpots()).then(() => setIsLoaded(true));
   }, [dispatch])
-  return (
+  return isLoaded && (
     <div className="spots-container">
       <div className="spots-cards-container">
         {spots?.map(spot => (
